f06012baa880d39dc9aca9393198eaf6bfc405c7
SoyQuillo <santiagoquintero190305@gmail.com>
2023-12-12 23:01:24 -0500

    first commit

---
+++ push/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+import { ref } from 'vue';
+
+let varRef = ref("")
+
+let objeto = ref([{
+  name: 'Santiago'
+},
+{name: 'Simon'}
+])
+
+function push () {
+
+ objeto.value.push({name:varRef.value})
+
+}
+</script>
+
+<template>
+  
+<div>
+  <div v-for="(obj, index) in objeto" :key="index">{{ obj.name }}</div>
+</div>
+
+<div> <input @keypress.enter="push()" v-model="varRef" type="text"> </div>
+</template>
+
+<style scoped>
+
+</style>
